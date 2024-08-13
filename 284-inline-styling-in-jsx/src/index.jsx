ac9224d615f4532581949422db1194f40fd3a35b
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2024-08-13 13:46:03 +0530

    fixed some typos

--- 284-inline-styling-in-jsx/src/index.jsx
+++ 284-inline-styling-in-jsx/src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+ 
 const coustomStyle = {
+    // CSS properties in JS Objects are written in CamelCase. & the values has to be a string.
     color: "#674188",
     backgroundColor: "#E2BFD9",
-    textAlign: "center"
+    textAlign: "center",
+    border: "1px solid black"
 }
+
+coustomStyle.color = "#982B1C"
 
 ReactDOM.render(
     <div>
@@ -15,7 +19,7 @@ ReactDOM.render(
     , document.getElementById("root")
 );
 
-
+// Style attribute value ("color: red") is just replaced with a Javascript Object ({color: red}) for inline style in JSX file.
 
 
 // ------------------------------------------------------------
